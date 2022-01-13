6ef59b7b0112ce4ea8ed64c2673092cb5b107eac
manuelMachuca <12200224@unmsm.edu.pe>
2022-01-12 23:59:28 -0500

    files

---
+++ src/main/java/com/manuel/app/App.java
@@ -0,0 +1,13 @@
+package com.manuel.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
