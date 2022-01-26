3d5f594dcca1493dcd31a1b49bfcc5fe2d18226b
manuelMachuca <12200224@unmsm.edu.pe>
2022-01-26 00:08:31 -0500

    Update AppTest.java

--- src/test/java/com/manuel/app/AppTest.java
+++ src/test/java/com/manuel/app/AppTest.java
@@ -15,7 +15,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        system.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
         assertTrue( true );
     }
 }
