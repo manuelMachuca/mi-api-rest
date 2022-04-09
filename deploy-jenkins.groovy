3b876082f2b686e14d4804831d3098c63a0e4dbe
manuelMachuca <12200224@unmsm.edu.pe>
2022-04-09 00:02:15 -0500

    Update deploy-jenkins.groovy

--- deploy-jenkins.groovy
+++ deploy-jenkins.groovy
@@ -34,27 +34,27 @@ node {
        //...
        echo 'Enviado a Veracode'
     }
-    stage('SonarQube analysis') {
-        
-        withMaven(
-        // Maven installation declared in the Jenkins "Global Tool Configuration"
-        maven: 'maven-3', // (1)
-        // Use `$WORKSPACE/.repository` for local repository folder to avoid shared repositories
-        mavenLocalRepo: '.repository', // (2)
-        // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
-        // We recommend to define Maven settings.xml globally at the folder level using
-        // navigating to the folder configuration in the section "Pipeline Maven Configuration / Override global Maven configuration"
-        // or globally to the entire master navigating to  "Manage Jenkins / Global Tools Configuration"
-        mavenSettingsConfig: '9ca6c49f-7398-46f3-b85b-d10bb70958a2' // (3)
-        ) {
+    
 
-        // Run the maven build
-        //sh "mvn clean verify"
-            withSonarQubeEnv(credentialsId: 'sonar_credential', installationName: 'SonarServer') { // You can override the credential to be used
-                sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
-            }
-        } // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe & FindBugs & SpotBugs reports...
-        
+    
+    stage('SonarQube analysis') {
+        withSonarQubeEnv('SonarServer') {
+        // Optionally use a Maven environment you've configured already
+             withMaven(
+                // Maven installation declared in the Jenkins "Global Tool Configuration"
+                maven: 'maven-3', // (1)
+                // Use `$WORKSPACE/.repository` for local repository folder to avoid shared repositories
+                mavenLocalRepo: '.repository', // (2)
+                // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
+                // We recommend to define Maven settings.xml globally at the folder level using
+                // navigating to the folder configuration in the section "Pipeline Maven Configuration / Override global Maven configuration"
+                // or globally to the entire master navigating to  "Manage Jenkins / Global Tools Configuration"
+                mavenSettingsConfig: '9ca6c49f-7398-46f3-b85b-d10bb70958a2' // (3)
+             ) {
+                  sh 'mvn clean package sonar:sonar'
+             }
+        }
+        //withSonarQubeEnv(credentialsId: 'sonar_credential', installationName: 'SonarServer') { // You can override the credential to be used
     }
     
 }
