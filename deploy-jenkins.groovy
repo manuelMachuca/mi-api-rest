2901857df3ca10cc2e292f9ec114974ad1a8c77c
manuelMachuca <12200224@unmsm.edu.pe>
2022-04-09 00:20:04 -0500

    Update deploy-jenkins.groovy

--- deploy-jenkins.groovy
+++ deploy-jenkins.groovy
@@ -38,7 +38,7 @@ node {
 
     
     stage('SonarQube analysis') {
-        withSonarQubeEnv('SonarServer', envOnly: true) {
+        withSonarQubeEnv(envOnly: true,'SonarServer') {
           // This expands the evironment variables SONAR_CONFIG_NAME, SONAR_HOST_URL, SONAR_AUTH_TOKEN that can be used by any script.
           println ${env.SONAR_HOST_URL} 
         }
